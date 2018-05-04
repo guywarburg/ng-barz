3fbf8c1ce19b194cce3ef0c430345d0777a5c64e
Guy Warburg <guy.warburg@gmail.com>
2018-05-04 20:33:28 +0300

    prettier and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,135 +1,156 @@
-import {Component} from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import {isNullOrUndefined} from "util";
-import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
-import {MdDialog} from '@angular/material';
-import {LoginComponent} from './login/login.component'
+import { Component } from "@angular/core";
+import {
+	AngularFireDatabase,
+	FirebaseListObservable
+} from "angularfire2/database";
+import { isNullOrUndefined } from "util";
+import { Observable } from "rxjs/Observable";
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from "firebase/app";
+import { MdDialog } from "@angular/material";
+import { LoginComponent } from "./login/login.component";
 
-import {tempDb} from '../temp';
+import { tempDb } from "../temp";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"]
 })
-export class AppComponent{
-  temp: any;
-  openDetailsModal: boolean = false;
-  selectedBar: any;
-  openMarkVModal: boolean = false;
-  openLoginModal: boolean = false;
-  searchVisible: boolean = false;
-  openUserAlreadyMakredModal: boolean = false;
-  center: any = {
-    lng: '',
-    lat: ''
-  };
-  allBars: any = [
-  {
-    title: 'Shamrock',
-    lng: 34.8627351,
-    lat: 32.2800516
-  },
-  {
-    title: 'Hanzl and Gretl',
-    lng: 34.8630365,
-    lat: 32.2787251
-  },
-  {
-    title: 'Beer Shop',
-    lng: 34.8589975,
-    lat: 32.2807231
-  },
-  ];
-  filteredBars: any;
-  barsObservable: FirebaseListObservable<any[]>;
-  userLoggedIn: boolean = false;
+export class AppComponent {
+	public openDetailsModal: boolean;
+	public selectedBar: any;
+	public openMarkVModal: boolean;
+	public openLoginModal: boolean;
+	public searchVisible: boolean;
+	public openUserAlreadyMakredModal: boolean;
+	public center: any = {
+		lng: "",
+		lat: ""
+	};
+	allBars: any = [
+		{
+			title: "Shamrock",
+			lng: 34.8627351,
+			lat: 32.2800516
+		},
+		{
+			title: "Hanzl and Gretl",
+			lng: 34.8630365,
+			lat: 32.2787251
+		},
+		{
+			title: "Beer Shop",
+			lng: 34.8589975,
+			lat: 32.2807231
+		}
+	];
+	filteredBars: any;
+	barsObservable$: FirebaseListObservable<any[]>;
+	public userLoggedIn: boolean;
 
-  constructor(db: AngularFireDatabase,
-              public afAuth: AngularFireAuth,
-              public dialog: MdDialog) {
-    this.barsObservable = db.list('/bars');
-    this.barsObservable.subscribe(state => {
-      if(!isNullOrUndefined(state)) {
-        this.allBars = [...state];
-        this.filteredBars = [...this.allBars];
-      }
-    });
-    this.afAuth.auth.onAuthStateChanged(user => {
-      if(user) {
-        this.userLoggedIn = true;
-      } else {
-        this.userLoggedIn = false;
-      }
-     });
-  }
+	constructor(
+		db: AngularFireDatabase,
+		public afAuth: AngularFireAuth,
+		public dialog: MdDialog
+	) {
+		this.setDefaultValues();
+		this.initDB(db);
+		this.checkAuthentication();
+	}
 
-  filterBars(value) {
-    if(!value) {
-      this.filteredBars = [...this.allBars];
-    } else {
-      this.filteredBars = this.allBars.filter(bar => {
-        if(typeof bar.title === 'string') {
-          return bar.title.toLowerCase().indexOf(value.toLowerCase()) > -1;
-        } else {
-          return true;
-        }
-        
-      })
-    }
-  }
+	setDefaultValues() {
+		this.openDetailsModal = false;
+		this.openMarkVModal = false;
+		this.openLoginModal = false;
+		this.searchVisible = false;
+		this.openUserAlreadyMakredModal = false;
+		this.userLoggedIn = false;
+	}
 
-  reCenterMap(bar) {
-    this.assignSelectedBar(bar);
-    let newCenter: any = {
-      lng: bar.lng,
-      lat: bar.lat
-    };
-    this.center = Object.assign({}, newCenter);
-  }
+	initDB(db: AngularFireDatabase) {
+		this.barsObservable$ = db.list("/bars");
+		this.barsObservable$.subscribe(state => {
+			if (!isNullOrUndefined(state)) {
+				this.allBars = [...state];
+				this.filteredBars = [...this.allBars];
+			}
+		});
+	}
 
-  assignSelectedBar(bar) {
-    this.selectedBar = bar;
-  }
+	checkAuthentication() {
+		this.afAuth.auth.onAuthStateChanged(user => {
+			if (user) {
+				this.userLoggedIn = true;
+			} else {
+				this.userLoggedIn = false;
+			}
+		});
+	}
 
-  openModal(e) {
-    this.openDetailsModal = true;
-  }
-  openMarkV(e) {
-    this.openDetailsModal = false;
-    console.log('e', e);
-    if(this.userLoggedIn) {
-      if(this.userAlreadyMarked(this.afAuth.auth.currentUser.uid, e)) {
-        this.openUserAlreadyMakredModal = true;
-      } else {
-        this.openMarkVModal = true; 
-      }
-    } else {
-      this.openLoginModal = true;
-    }
+	filterBars(value) {
+		if (!value) {
+			this.filteredBars = [...this.allBars];
+		} else {
+			this.filteredBars = this.allBars.filter(bar => {
+				if (typeof bar.title === "string") {
+					return bar.title.toLowerCase().indexOf(value.toLowerCase()) > -1;
+				} else {
+					return true;
+				}
+			});
+		}
+	}
 
-  }
-  closeMarkVModal() {
-    this.openMarkVModal = false;
-  }
-  handleLoginSucess() {
-    this.openLoginModal = false;
-    if(!isNullOrUndefined(this.selectedBar)){
-      this.openMarkVModal = true;
-    }
-  }
+	reCenterMap(bar) {
+		this.assignSelectedBar(bar);
+		const newCenter = {
+			lng: bar.lng,
+			lat: bar.lat
+		};
+		this.center = { ...newCenter };
+	}
 
-  toggleSearch() {
-    this.searchVisible = !this.searchVisible;
-  }
-  userAlreadyMarked(uid: string, relevantRanks: any[]): boolean {
-    return relevantRanks.filter(item => {
-      return item.uid === uid;
-    }).length > 0;
-  }
-  openDialog() {
-    // this.dialog.open(LoginComponent);
-  }
+	assignSelectedBar(bar) {
+		this.selectedBar = bar;
+	}
+
+	openModal(e) {
+		this.openDetailsModal = true;
+	}
+
+	openMarkV(e) {
+		this.openDetailsModal = false;
+		if (this.userLoggedIn) {
+			if (this.userAlreadyMarked(this.afAuth.auth.currentUser.uid, e)) {
+				this.openUserAlreadyMakredModal = true;
+			} else {
+				this.openMarkVModal = true;
+			}
+		} else {
+			this.openLoginModal = true;
+		}
+	}
+
+	closeMarkVModal() {
+		this.openMarkVModal = false;
+	}
+
+	handleLoginSucess() {
+		this.openLoginModal = false;
+		if (!isNullOrUndefined(this.selectedBar)) {
+			this.openMarkVModal = true;
+		}
+	}
+
+	toggleSearch() {
+		this.searchVisible = !this.searchVisible;
+	}
+	userAlreadyMarked(uid: string, relevantRanks: any[]): boolean {
+		return (
+			relevantRanks.filter(item => {
+				return item.uid === uid;
+			}).length > 0
+		);
+	}
 }
