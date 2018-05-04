119ac35eeba574f53bf4e6cb397b42b03fd2094c
Guy Warburg <guy.warburg@gmail.com>
2018-05-04 20:33:05 +0300

    prettier

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,68 +1,79 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
-import {mapColors} from "../../shared/config";
+import {
+	Component,
+	EventEmitter,
+	Input,
+	OnChanges,
+	OnInit,
+	Output,
+	SimpleChanges
+} from "@angular/core";
+import { mapColors } from "../../shared/config";
 
 @Component({
-  selector: 'map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+	selector: "app-map",
+	templateUrl: "./map.component.html",
+	styleUrls: ["./map.component.scss"]
 })
 export class MapComponent implements OnInit, OnChanges {
-  @Input() bars: any;
-  @Input() center: any;
-  @Output() selectedBar: EventEmitter<any> = new EventEmitter();
-  @Output() openDialog: EventEmitter<any> = new EventEmitter();
+	@Input() bars: any;
+	@Input() center: any;
+	@Output() selectedBar: EventEmitter<any> = new EventEmitter();
+	@Output() openDialog: EventEmitter<any> = new EventEmitter();
 
-  public lat: number;
-  public lng: number;
+	public lat: number;
+	public lng: number;
 
-  public myLat: number;
-  public myLng: number;
+	public myLat: number;
+	public myLng: number;
 
-  public zoom: number;
+	public zoom: number;
 
-  public iconPath: string;
+	public iconPath: string;
 
-  public mapOptions: any[];
+	public mapOptions: any[];
 
-  constructor() {
-    this.setDefaultValues();
-    this.iconPath = '../assets/images/beer-icon-white.png';
-    this.mapOptions = mapColors;
-  }
+	constructor() {
+		this.setDefaultValues();
+		this.iconPath = "../assets/images/beer-icon-white.png";
+		this.mapOptions = mapColors;
+	}
 
-  ngOnInit() {
-    this.setMyLocation();
-    this.centerMapToLocation();
-  }
+	ngOnInit() {
+		this.setMyLocation();
+		this.centerMapToLocation();
+	}
 
-  ngOnChanges(changes: SimpleChanges) {
-    if(changes.center && changes.center.previousValue !== changes.center.currentValue) {
-      this.lng = this.center.lng ? this.center.lng: this.lng;
-      this.lat = this.center.lat ? this.center.lat : this.lat;
-    }
-  }
+	ngOnChanges(changes: SimpleChanges) {
+		if (
+			changes.center &&
+			changes.center.previousValue !== changes.center.currentValue
+		) {
+			this.lng = this.center.lng ? this.center.lng : this.lng;
+			this.lat = this.center.lat ? this.center.lat : this.lat;
+		}
+	}
 
-  setDefaultValues() {
-    this.myLat = 32.0767365;
-    this.myLng = 34.7800686;
-    this.zoom = 16;
-  }
+	setDefaultValues() {
+		this.myLat = 32.0767365;
+		this.myLng = 34.7800686;
+		this.zoom = 16;
+	}
 
-  setMyLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.myLng = position.coords.longitude;
-        this.myLat = position.coords.latitude;
-      });
-    }
-  }
+	setMyLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(position => {
+				this.myLng = position.coords.longitude;
+				this.myLat = position.coords.latitude;
+			});
+		}
+	}
 
-  centerMapToLocation() {
-    this.lat = this.myLat;
-    this.lng = this.myLng;
-  }
-  handleMarkerClick(bar) {
-    this.selectedBar.emit(bar);
-    this.openDialog.emit(bar);
-  }
+	centerMapToLocation() {
+		this.lat = this.myLat;
+		this.lng = this.myLng;
+	}
+	handleMarkerClick(bar) {
+		this.selectedBar.emit(bar);
+		this.openDialog.emit(bar);
+	}
 }
